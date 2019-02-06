28bbf30fcbe0095da5616db4ea28e1dd96210d3e
Charles Brandt <brandtc@bloomington.in.gov>
2019-02-06 12:19:42 -0500

    first pass at adding in user authentication

--- account_tracker/urls.py
+++ account_tracker/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import TemplateView
 from .routers import router
 
 urlpatterns = [
+    path('accounts/', include('django.contrib.auth.urls')),
+    # path(r'^login/$', auth_views.login, name='login'),
+    # path(r'^logout/$', auth_views.logout, name='logout'),
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls))
+    path('api/', include(router.urls)),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
