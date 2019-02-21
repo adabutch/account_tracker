6cf6c09adb8dc4afb3385888627ca2d182d5ef7f
adabutch <adabutch1@gmail.com>
2019-02-21 08:55:40 -0500

    api query params for service profile dept/group

--- default_profile/viewsets.py
+++ default_profile/viewsets.py
@@ -1,8 +1,19 @@
 from rest_framework import viewsets
+from django_filters import rest_framework as filters
 from .models import DefaultProfile
 from .serializers import DefaultProfileSerializer
+
+class ProfileFilter(filters.FilterSet):
+    department_id = filters.NumberFilter(field_name="department_id")
+    group_id = filters.NumberFilter(field_name="group_id")
+
+    class Meta:
+        model = DefaultProfile
+        fields = ['department_id', 'group_id']
 
 
 class DefaultProfileViewSet(viewsets.ModelViewSet):
     queryset = DefaultProfile.objects.all()
     serializer_class = DefaultProfileSerializer
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_class = ProfileFilter
