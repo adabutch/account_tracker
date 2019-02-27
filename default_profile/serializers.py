677c065abad991d7e4f95d73291fbea8d48d7d9b
Charles Brandt <brandtc@bloomington.in.gov>
2019-02-27 15:46:33 -0500

    returning full service details with a default profile

--- default_profile/serializers.py
+++ default_profile/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
 from .models import DefaultProfile
-
+from service.serializers import ServiceSerializer
 
 class DefaultProfileSerializer(serializers.ModelSerializer):
+
+    services = ServiceSerializer(many=True, read_only=True)
+
     class Meta:
         model = DefaultProfile
         fields = '__all__'
