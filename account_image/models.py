d2145e9c7d2065b71d6faee013cde8f2abfd2947
Charles Brandt <brandtc@bloomington.in.gov>
2019-03-07 16:34:50 -0500

    image type is not necessary on account image now that it holds both

--- account_image/models.py
+++ account_image/models.py
@@ -9,9 +9,6 @@ class AccountImage(models.Model):
     creator = models.ForeignKey(
         User, on_delete=models.SET_NULL, blank=True, null=True)
 
-    # is it the full size version? cropped?
-    image_type = models.CharField(max_length=50)
-
     created = models.DateTimeField(auto_now_add=True)
     # any associated logged actions could trigger an update to updated
     updated = models.DateTimeField(auto_now=True)
