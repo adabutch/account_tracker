a3c7cc16ee79c629e3fb926975265e369176a7e0
adabutch <adabutch1@gmail.com>
2019-03-07 14:05:04 -0500

    account req. request_status filter

--- account_request/viewsets.py
+++ account_request/viewsets.py
@@ -1,8 +1,18 @@
 from rest_framework import viewsets
+from django_filters import rest_framework as filters
 from .models import AccountRequest
 from .serializers import AccountRequestSerializer
+
+class ProfileFilter(filters.FilterSet):
+    request_status = filters.CharFilter(field_name="request_status")
+
+    class Meta:
+        model = AccountRequest
+        fields = ['request_status']
 
 
 class AccountRequestViewSet(viewsets.ModelViewSet):
     queryset = AccountRequest.objects.all()
     serializer_class = AccountRequestSerializer
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_class = ProfileFilter
