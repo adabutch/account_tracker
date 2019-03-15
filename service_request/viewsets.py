b6f4e36573881f8eb01ab330b42f1993037f6268
adabutch <adabutch1@gmail.com>
2019-03-15 09:26:04 -0400

    query service req via account req ID

--- service_request/viewsets.py
+++ service_request/viewsets.py
@@ -1,8 +1,18 @@
 from rest_framework import viewsets
+from django_filters import rest_framework as filters
 from .models import ServiceRequest
 from .serializers import ServiceRequestSerializer
+
+class ServiceRequestFilter(filters.FilterSet):
+    account_request = filters.CharFilter(field_name="account_request")
+
+    class Meta:
+        model  = ServiceRequest
+        fields = ['account_request']
 
 
 class ServiceRequestViewSet(viewsets.ModelViewSet):
     queryset = ServiceRequest.objects.all()
     serializer_class = ServiceRequestSerializer
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_class = ServiceRequestFilter
