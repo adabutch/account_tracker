5e173aa260065fe73d88405c16864c78e3037541
adabutch <adabutch1@gmail.com>
2019-03-21 15:44:41 -0400

    init home search III

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -3,12 +3,17 @@
     <headerComponent />
 
     <div class="search">
-      <fn1-input v-model="acctReqSearch"
+      <div class="wrapper">
+        <fn1-input v-model="acctReqSearch"
                  label="Account Request Search"
                  placeholder="Search by Name, Department or Group"
                  name="acct-req-search"
                  id="acct-req-search" />
-      <!-- {{filteredAcctReqs.length}} -->
+
+        <p v-if="filteredAcctReqs.length && acctReqSearch !== ''">
+          {{filteredAcctReqs.length}} results
+        </p>
+      </div>
     </div>
 
     <div class="results-wrapper">
@@ -24,7 +29,7 @@
         <h4 v-if="!acctReqSearch">Search results will appear here.</h4>
 
         <div class="no-results" v-if="!filteredAcctReqs.length">
-          <h4>No results for <strong>"{{acctReqSearch}}"</strong>.</h4>
+          <h4>No results for, <strong>"{{acctReqSearch}}"</strong>.</h4>
           <fn1-button @click.native="goCreateAR">New Account Request</fn1-button>
         </div>
 
@@ -155,12 +160,25 @@ export default {
     padding: 50px 0 0 0;
     z-index: 1;
 
-    /deep/ .field-group {
+    .wrapper {
+      position: relative;
       border-top-left-radius: ($radius-default * 2);
       border-top-right-radius: ($radius-default * 2);
       background: rgba(255, 255, 255, 0.08%);
       padding: 50px 100px;
       width: 750px;
+      margin: 0;
+
+      p {
+        position: absolute;
+        bottom: 15px;
+        color: white;
+        font-style: italic;
+      }
+    }
+
+    /deep/ .field-group {
+      width: 100%;
       margin: 0;
 
       label {
@@ -211,6 +229,7 @@ export default {
 
       h4 {
         // background-color: green;
+        color: lighten($text-color, 25%);
         width: 100%;
         padding: 20px;
         text-align: center;
