9147c277fd7abba79dcfa2bff3e0cd4ab546fdd6
adabutch <adabutch1@gmail.com>
2019-04-02 16:11:13 -0400

    init service profile managers que page

--- frontend/pages/services/index.vue
+++ frontend/pages/services/index.vue
@@ -1,34 +1,97 @@
 <template>
   <div class="table-wrapper">
-    {{mgrProfiles}}
-    <!-- <fn1-tabs>
-      <fn1-tab :name="`New (` + [[ 'yo' ]] + `)`"
-               :selected="true">
+    <fn1-tabs>
+      <fn1-tab :name="`New`" :selected="true">
 
         <div class="title-row">
-          <h4>Services Requests</h4>
+          <h4>New <strong>Services Requests</strong> awaiting action.</h4>
+
+          <div class="filters">
+            <fn1-badge
+              v-for="s, i in requestStatuses"
+              :key="i"
+              :class="s">
+              {{s}}
+            </fn1-badge>
+          </div>
         </div>
 
         <table v-if="true">
           <caption class="sr-only">All User Requests</caption>
           <thead>
             <tr>
-              <th scope="col">Status</th>
-              <th scope="col">Name</th>
-              <th scope="col">Dept.</th>
+              <th scope="col">Acct. Req.</th>
+              <th scope="col">Service</th>
+              <th scope="col">Change Type</th>
+              <th scope="col">Req. Status</th>
               <th scope="col">Created</th>
+              <th scope="col">Requested</th>
+              <th scope="col">Updated</th>
               <th scope="col">Actions</th>
             </tr>
           </thead>
 
           <tbody>
-            <tr>
-              <th>hi</th>
+            <tr v-for="s, i in mgrServiceReqs" :key="i">
+              <th>{{s.account_request}}/{{s.id}}</th>
+              <th>{{s.service}}</th>
+              <th>{{s.type_of_change}}</th>
+              <th>{{s.request_status}}</th>
+              <th>{{s.created}}</th>
+              <th>{{s.requested}}</th>
+              <th>{{s.updated}}</th>
+              <th>actions</th>
             </tr>
           </tbody>
         </table>
       </fn1-tab>
-    </fn1-tabs> -->
+
+      <fn1-tab :name="`Pending`">
+
+        <div class="title-row">
+          <h4>New <strong>Services Requests</strong> awaiting action.</h4>
+
+          <div class="filters">
+            <fn1-badge
+              v-for="s, i in requestStatuses"
+              :key="i"
+              :class="s">
+              {{s}}
+            </fn1-badge>
+          </div>
+        </div>
+
+        <table v-if="true">
+          <caption class="sr-only">All User Requests</caption>
+          <thead>
+            <tr>
+              <th scope="col">Acct. Req.</th>
+              <th scope="col">Service</th>
+              <th scope="col">Change Type</th>
+              <th scope="col">Req. Status</th>
+              <th scope="col">Created</th>
+              <th scope="col">Requested</th>
+              <th scope="col">Updated</th>
+              <th scope="col">Actions</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr v-for="s, i in mgrServiceReqs" :key="i">
+              <th>{{s.account_request}}/{{s.id}}</th>
+              <th>{{s.service}}</th>
+              <th>{{s.type_of_change}}</th>
+              <th>{{s.request_status}}</th>
+              <th>{{s.created}}</th>
+              <th>{{s.requested}}</th>
+              <th>{{s.updated}}</th>
+              <th>actions</th>
+            </tr>
+          </tbody>
+        </table>
+      </fn1-tab>
+
+    </fn1-tabs>
   </div>
 </template>
 
@@ -46,24 +109,43 @@ export default {
   middleware:       'authenticated',
   components: {
   },
-  data() {
-    return {
-      mgrID:        null,
-      mgrProfiles:  [],
-    }
-  },
   mounted() {
+    this.getServices;
+    this.getServiceRequests;
     this.mgrID = this.authUser.id;
     this.getMgrProfiles;
+    this.mgrServices;
+    this.filterMgrServiceReqs;
+  },
+  data() {
+    return {
+      services:     [],
+      serviceReqs:  [],
+      mgrServiceReqs: [],
+      mgrID:        null,
+      mgrProfiles:  [],
+      mgrServiceProfiles: [],
+    }
   },
   methods: {},
   computed: {
     ...mapFields([
-      'auth.authUser'
+      'auth.authUser',
+      'requestStatuses'
     ]),
+    getServices() {
+      this.$axios
+      .get(`${process.env.api}${process.env.service}?limit=5000`)
+      .then((res) => {
+        this.services = res.data.results;
+      })
+      .catch((e) => {
+        console.log(e);
+      })
+    },
     getMgrProfiles() {
       this.$axios
-      .get(`${process.env.api}${process.env.serviceManager}?manager=${this.mgrID}&limit=1000`)
+      .get(`${process.env.api}${process.env.serviceManager}?manager=${this.mgrID}&limit=5000`)
       .then((res) => {
         let data = res.data.results;
         data.forEach((service) => {
@@ -74,7 +156,28 @@ export default {
         console.log(e);
       })
     },
-
+    getServiceRequests() {
+      this.$axios
+      .get(`${process.env.api}${process.env.serviceReq}?limit=5000`)
+      .then((res) => {
+        this.serviceReqs = res.data.results;
+      })
+      .catch((e) => {
+        console.log(e);
+      })
+    },
+    mgrServices() {
+      let results = this.services.filter((item) => {
+        return this.mgrProfiles.indexOf(item.id) >= 0;
+      });
+      return this.mgrServiceProfiles = results;
+    },
+    filterMgrServiceReqs() {
+      let results = this.serviceReqs.filter((item) => {
+        return this.mgrProfiles.indexOf(item.service) >= 0;
+      });
+      return this.mgrServiceReqs = results;
+    }
   }
 }
 </script>
@@ -139,12 +242,27 @@ export default {
             align-items: center;
             margin: 30px 0 20px 0;
             min-height: 25px;
+            width: 100%;
             // background-color: red;
 
             h4 {
+              width: fit-content;
               color: $text-color;
               font-size: 18px;
               line-height: 18px;
+            }
+
+            .filters {
+              margin-left: auto;
+              display: flex;
+
+              .badge {
+                margin: 0 10px 0 0;
+
+                &:last-of-type {
+                  margin: 0;
+                }
+              }
             }
 
             button {
