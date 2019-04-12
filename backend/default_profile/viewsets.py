9c9ae059f12d1a88ab6bb22c65157832b4905ebe
Charles Brandt <brandtc@bloomington.in.gov>
2019-04-12 12:41:17 -0400

    new methods to add or remove a service to a default profile

--- backend/default_profile/viewsets.py
+++ backend/default_profile/viewsets.py
@@ -2,6 +2,10 @@ from rest_framework import viewsets
 from django_filters import rest_framework as filters
 from .models import DefaultProfile
 from .serializers import DefaultProfileSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from service.models import Service
 
 class ProfileFilter(filters.FilterSet):
     department_id = filters.NumberFilter(field_name="department_id")
@@ -17,3 +21,28 @@ class DefaultProfileViewSet(viewsets.ModelViewSet):
     serializer_class = DefaultProfileSerializer
     filter_backends = (filters.DjangoFilterBackend,)
     filterset_class = ProfileFilter
+
+    # https://stackoverflow.com/questions/50425262/django-rest-framework-pass-extra-parameter-to-actions
+    @action(detail=True, methods=['post'], url_path='add_service/(?P<service_pk>[^/.]+)')
+    def add_service(self, request, service_pk, pk=None):
+        """
+        TODO:
+        restrict access to only certain admin levels
+        """
+        profile = self.get_object()
+        service = get_object_or_404(Service, pk=service_pk)
+        profile.services.add(service)
+        profile.save()
+        return Response("success")
+
+    @action(detail=True, methods=['post'], url_path='remove_service/(?P<service_pk>[^/.]+)')
+    def remove_service(self, request, service_pk, pk=None):
+        """
+        TODO:
+        restrict access to only certain admin levels
+        """
+        profile = self.get_object()
+        service = get_object_or_404(Service, pk=service_pk)
+        profile.services.remove(service)
+        profile.save()
+        return Response("success")
